40d1519142295678975c6c32d40f99f9d5d7161a
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-05-16 16:00:44 -0400

    new project Search bar for gifs. Static state

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import SearchBar from './search-bar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+
+          </div>
+        </div>
+        <div className="right-scene"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,40 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-// const Hello = ({ name }) => {
-//   return (
-//     <div> Hello, {name}</div>
-//   );
-// };
-
-class Hello extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      clicked: false,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter + 1
-    });
-  }
-
-  render() {
-    return (
-      <div className={this.state.clicked ? "clicked" : null} onClick={this.handleClick}>
-        Hello {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-}
+import App from "./components/app";
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Laura" />, root);
+  // ReactDOM.render(<Hello name="Laura" />, root);
+  ReactDOM.render(<App />, root);
 }
