36bd427d49d845b803108333864d45a00ec8beda
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-05-16 15:07:32 -0400

    introduction to states in React

--- src/index.jsx
+++ src/index.jsx
@@ -13,19 +13,23 @@ class Hello extends Component {
   constructor (props) {
     super(props);
 
-    this.state = { clicked: false };
+    this.state = {
+      clicked: false,
+      counter: 0
+    };
   }
 
   handleClick = () => {
     this.setState({
-      clicked: !this.state.clicked
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
     });
   }
 
   render() {
     return (
       <div className={this.state.clicked ? "clicked" : null} onClick={this.handleClick}>
-        Hello {this.props.name}
+        Hello {this.props.name} {this.state.counter}
       </div>
     );
   }
