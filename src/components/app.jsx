28942ba586eaff329a962736942288448e83846f
Laura Hardy-Dessources <laura.hardydes@gmail.com>
2022-05-30 16:09:38 -0400

    giphy-api added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,35 @@
+// External imports
 import React, { Component } from 'react';
+import giphy from "giphy-api";
+
+// Internal imports
 import Gif from './gif';
 import SearchBar from './search-bar';
 import GifsList from './gifs_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT1XGx3mIWL2J6PMWc"
+    }
+  }
+
+  search =(query)=> {
+    giphy("VQEiTyY8Lk9HAByZ96ZU14bSCN3dUn7x").search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      // Res contains gif data!
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
   render() {
     const gifs = [
       {id: "xT1XGx3mIWL2J6PMWc"},
@@ -14,11 +40,11 @@ class App extends Component {
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="xT1XGx3mIWL2J6PMWc"/>
+            <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
         <div className="right-scene">
-          <GifsList gifs={gifs} />
+          <GifsList gifs={this.state.gifs} />
         </div>
       </div>
     );
